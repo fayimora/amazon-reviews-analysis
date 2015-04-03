3193a447aa2065b77fbb1a125e19b8637d4db59b
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-04-03 06:54:37 -0400

    Add initial implementation of topic modelling

---
+++ topic_modelling.py
@@ -0,0 +1,32 @@
+import logging
+from sklearn.feature_extraction.text import CountVectorizer
+from gensim.corpora import Dictionary
+from gensim.models.lsimodel import LsiModel
+
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+
+def train_lsi_model(corpus, dictionary):
+    id2word = {}
+    for word in dictionary.token2id:
+        id2word[dictionary.token2id[word]] = word
+
+    lsi = LsiModel(corpus=corpus, id2word=id2word)
+    return lsi
+
+
+def get_tokeniser(ngram_range=(1,1), stop_words='english'):
+    vect = CountVectorizer(ngram_range=ngram_range, stop_words=stop_words)
+    return vect.build_tokenizer()
+
+if __name__ == '__main__':
+    lines = open('data/lda_in_noid.txt').readlines()
+    tokeniser = get_tokeniser()
+    texts = map(lambda s: tokeniser(s), lines)
+    dictionary = Dictionary(texts)
+    corpus = [dictionary.doc2bow(text) for text in texts]
+    lsa = train_lsi_model(corpus, dictionary)
+
+
+
