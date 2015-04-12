8aa42bc5316e69f89443b37860309465dce32a4a
Fayimora Femi-Balogun <fayi@fayimora.com>
2015-04-12 12:41:04 +0100

    Major updates
    * only make 1 pass through the dataset.
    * models and corpus and now saved(pickled) to disk for easy access later
    * As a result of the above, we now load a saved corpora and dictionary if already pickled

--- topic_modelling.py
+++ topic_modelling.py
@@ -1,5 +1,7 @@
 import logging
 import re
+import cPickle
+from os.path import isfile
 from sklearn.feature_extraction.text import CountVectorizer
 from gensim.corpora import Dictionary
 from gensim.models.lsimodel import LsiModel
@@ -20,7 +22,7 @@ def id2word(dictionary):
 
 def train_lda_model(corpus, dictionary, num_topics):
     lda = LdaModel(corpus=corpus, num_topics=num_topics, id2word=id2word(dictionary), \
-                    alpha='auto', update_every=1, chunksize=100, passes=2, iterations=50)
+                    alpha='auto', update_every=1, chunksize=10000, passes=1, iterations=50)
     return lda
 
 
@@ -38,21 +40,56 @@ def prettify(topics):
     return map(lambda ts: re.sub("\\s\+", ",", ts), map(lambda t: re.sub("(\d*\.\d*\*)", "", t), topics))
 
 
+def save(data, file):
+    fo = open(file, 'w')
+    cPickle.dump(data, fo, protocol=2)
+    fo.close()
+
+
+def load(file):
+    fo = open(file, 'rb')
+    data = cPickle.load(fo)
+    fo.close()
+    return data
+
+
 if __name__ == '__main__':
-    logging.info("Loading data from text file")
-    lines = open('data/topics_in.txt').readlines()
+    dataset_name = "electronics"
+    tfidf_corpus_path = 'data/models/%s_tfidf_corpus.pkl' % dataset_name
+    tfidf_corpus = None
 
-    logging.info("Tokenising data")
-    tokeniser = get_tokeniser()
-    texts = map(lambda s: tokeniser(s), lines)
+    if not isfile(tfidf_corpus_path):
+        logging.warn("No saved models available. Generating corpus. This might take a while...")
 
-    logging.info("Generating bag of words")
-    dictionary = Dictionary(texts)
-    corpus = [dictionary.doc2bow(text) for text in texts]
+        logging.info("Loading data from text file")
+        lines = open('data/%s_topics_in.txt' % dataset_name).readlines()
 
-    logging.info("Applying Tfidf model to corpus")
-    tfidf = TfidfModel(corpus)
-    tfidf_corpus = [tfidf[c] for c in corpus]
+        logging.info("Tokenising data")
+        tokeniser = get_tokeniser()
+        texts = map(lambda s: tokeniser(s), lines)
+
+        # logging.info("Saving corpus texts")
+        # save(texts, 'data/models/%s_texts.pkl' % dataset_name)
+
+        logging.info("Generating bag of words")
+        dictionary = Dictionary(texts)
+        save(dictionary, 'data/models/%s_dict.pkl' % dataset_name)
+        corpus = [dictionary.doc2bow(text) for text in texts]
+
+        # logging.info("Saving bag of words")
+        # save(corpus, 'data/models/%s_corpus.pkl' % dataset_name)
+
+        logging.info("Applying Tfidf model to corpus")
+        tfidf = TfidfModel(corpus)
+        tfidf_corpus = [tfidf[c] for c in corpus]
+
+        logging.info("Saving TF-IDF transformed corpus")
+        save(tfidf_corpus, tfidf_corpus_path)
+    else:
+        logging.info("Loading saved TF-IDF dictionary")
+        dictionary = load('data/models/%s_dict.pkl' % dataset_name)
+        logging.info("Loading saved TF-IDF corpus")
+        tfidf_corpus = load(tfidf_corpus_path)
 
     # logging.info("Training LSI model")
     # lsi = train_lsi_model(tfidf_corpus, dictionary)
@@ -60,7 +97,7 @@ if __name__ == '__main__':
     # lsi_topic_distribution = [l for l in lsi[tfidf_corpus]]
 
     logging.info("Training LDA model")
-    lda = train_lda_model(tfidf_corpus, dictionary, 20)
+    lda = train_lda_model(tfidf_corpus, dictionary, 30)
     lda_raw_topics = lda.show_topics(-1)
     lda_pretty_topics = prettify(lda.show_topics(-1))
     lda_topic_distribution = [l for l in lda[tfidf_corpus]]
